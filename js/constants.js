ac1378d2de74f746d4edb72e10afd8ec088a1154
MarinaLagarnikova <m.lagarnikova@dvizh.io>
2026-03-27 10:24:08 +0300

    feat: add dino physics constants (jump/gravity/fast-fall)

--- js/constants.js
+++ js/constants.js
@@ -11,3 +11,6 @@ export const DINO_FRAME_DURATION = 100; // ms per animation frame
 export const DINO_RUN_FRAME_COUNT = 3;  // number of run animation frames
 export const BG_ELEMENT_COUNT = 5;     // number of background parallax elements
 export const GROUND_DASH_CYCLE = 40;   // 2x dash(8) + gap(12) = 40px wrap cycle for ground line
+export const DINO_JUMP_VY = -600;          // initial upward velocity, px/s
+export const DINO_GRAVITY = 1800;          // downward accel, px/s²
+export const DINO_FAST_FALL_GRAVITY = 3600; // fast-fall accel (↓ in air), px/s²
